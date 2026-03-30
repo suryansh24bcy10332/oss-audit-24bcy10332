df13f35323667e89a8c964a22a3615d79a57a5e8
suryansh24bcy10332 <suryansh.24bcy10332@vitbhopal.ac.in>
2026-03-30 23:36:47 +0530

    Update Script 2.sh

--- Script 2.sh
+++ Script 2.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 # Script 2: FOSS Package Inspector
+# This script checks if a package is installed and shows basic details
 
-PACKAGE="git"
+PACKAGE="git"   # Name of package to check
 
+# ----------- CHECK IF INSTALLED -----------
+# command -v checks if the command exists in system PATH
 if command -v $PACKAGE &>/dev/null; then
     echo "$PACKAGE is installed."
+    
+    # Display version of Git
     git --version
 else
     echo "$PACKAGE is NOT installed."
 fi
 
+# ----------- CASE STATEMENT -----------
+# Prints a short description based on package name
 case $PACKAGE in
     git) echo "Git: distributed version control built for open collaboration" ;;
     apache) echo "Apache: powers a huge portion of the web" ;;
     mysql) echo "MySQL: database behind countless applications" ;;
     vlc) echo "VLC: media player that supports almost everything" ;;
+    *) echo "Unknown package" ;;   # Default case
 esac
